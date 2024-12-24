bf9a55352a57aafb2ad0d91f9c63d6adc830ee35
bestisblessed <tyler.durette@gmail.com>
2024-12-24 15:55:22 -0500

    im dumb

--- Streamlit/pages/05_🥊_Fighters.py
+++ Streamlit/pages/05_🥊_Fighters.py
@@ -8,9 +8,11 @@ df_fighter_data = st.session_state['df_fighter_data']
 df_event_data = st.session_state['df_event_data']
 
 # Displaying fighters
-fighter_names = df_fighter_data['Fighter'].unique()
+# fighter_names = df_fighter_data['Fighter'].unique()
+fighter_names = df_fighter_data['fighter'].unique()
 selected_fighter = st.selectbox('Select a fighter:', fighter_names)
 
 # Filter fighter data
-fighter_profile = df_fighter_data[df_fighter_data['Fighter'] == selected_fighter]
+# fighter_profile = df_fighter_data[df_fighter_data['Fighter'] == selected_fighter]
+fighter_profile = df_fighter_data[df_fighter_data['fighter'] == selected_fighter]
 st.write(fighter_profile[['Fighter', 'Nickname', 'Birth Date', 'Nationality', 'Hometown', 'Association', 'Weight Class', 'Height', 'Wins', 'Losses']])
