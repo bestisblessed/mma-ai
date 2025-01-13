e47aa457c9c985af165bf2e6360d39920816731b
bestisblessed <tyler.durette@gmail.com>
2025-01-13 05:59:19 -0500

    im dumb

--- Streamlit/pages/04_🥊_Fighters.py
+++ Streamlit/pages/04_🥊_Fighters.py
@@ -13,7 +13,7 @@ selected_fighter = st.selectbox('Select a fighter:', fighter_names_display)
 fighter_profile = df_fighter_data[df_fighter_data['fighter'] == selected_fighter.lower()]
 # st.write(fighter_profile[['fighter', 'nickname', 'birth date', 'nationality', 'hometown', 'association', 'weight class', 'height', 'wins', 'losses']])
 # st.write(fighter_profile)
-st.header(f"Profile for {selected_fighter.title()}")  # Capitalize for display
+# st.header(f"Profile for {selected_fighter.title()}")  # Capitalize for display
 columns_to_display = [
     'fighter', 'nickname', 'birth date', 'nationality', 'hometown', 
     'association', 'weight class', 'reach', 'height_in_inches', 'height', 'wins', 'losses', 'current_layoff',
