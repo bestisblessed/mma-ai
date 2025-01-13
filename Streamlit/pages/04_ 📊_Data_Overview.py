f6580340dd2a0417363713970a462890cd9b4cbd
bestisblessed <tyler.durette@gmail.com>
2025-01-13 04:53:21 -0500

    FIXING FIGHTER PAGE

--- Streamlit/pages/04_ 📊_Data_Overview.py
+++ Streamlit/pages/04_ 📊_Data_Overview.py
@@ -1,25 +1,26 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 
 st.title("Data Overview")
 
 df_fighter_data = st.session_state['df_fighter_data']
 df_event_data = st.session_state['df_event_data']
 
-# Dropdown button for datasets 
-option = st.selectbox('Choose a DataFrame to display:', ('None', 'Fighters', 'Fights'))
+# Create two columns
+col1, col2 = st.columns(2)
 
-if option == 'Fighters':
+# Display Fighters DataFrame
+with col1:
     st.header('Fighters DataFrame')
     st.write('First few rows:')
     st.dataframe(df_fighter_data)
     st.write('Column names:')
-    st.write(df_fighter_data.columns.tolist())
+    st.write(", ".join(df_fighter_data.columns))  # Print column names in a readable way
 
-elif option == 'Fights':
+# Display Fights DataFrame
+with col2:
     st.header('Fights DataFrame')
     st.write('First few rows:')
     st.dataframe(df_event_data)
     st.write('Column names:')
-    st.write(df_event_data.columns.tolist())
+    st.write(", ".join(df_event_data.columns))  # Print column names in a readable way
--- Streamlit/pages/05_🥊_Fighters.py
+++ Streamlit/pages/05_🥊_Fighters.py
@@ -7,12 +7,21 @@ st.title('Fighters')
 df_fighter_data = st.session_state['df_fighter_data']
 df_event_data = st.session_state['df_event_data']
 
-# Displaying fighters
-# fighter_names = df_fighter_data['Fighter'].unique()
 fighter_names = df_fighter_data['fighter'].unique()
-selected_fighter = st.selectbox('Select a fighter:', fighter_names)
-
-# Filter fighter data
-# fighter_profile = df_fighter_data[df_fighter_data['Fighter'] == selected_fighter]
-fighter_profile = df_fighter_data[df_fighter_data['fighter'] == selected_fighter]
-st.write(fighter_profile[['Fighter', 'Nickname', 'Birth Date', 'Nationality', 'Hometown', 'Association', 'Weight Class', 'Height', 'Wins', 'Losses']])
+fighter_names_display = [name.title() for name in fighter_names]
+selected_fighter = st.selectbox('Select a fighter:', fighter_names_display)
+fighter_profile = df_fighter_data[df_fighter_data['fighter'] == selected_fighter.lower()]
+# st.write(fighter_profile[['fighter', 'nickname', 'birth date', 'nationality', 'hometown', 'association', 'weight class', 'height', 'wins', 'losses']])
+# st.write(fighter_profile)
+st.header(f"Profile for {selected_fighter.title()}")  # Capitalize for display
+columns_to_display = [
+    'fighter', 'nickname', 'birth date', 'nationality', 'hometown', 
+    'association', 'weight class', 'reach', 'height_in_inches', 'height', 'wins', 'losses', 'current_layoff',
+    'current_win_streak', 'recent_win_rate_7fights', 'recent_win_rate_5fights', 'recent_win_rate_3fights'
+]
+for column in columns_to_display:
+    value = fighter_profile[column].values[0]
+    if isinstance(value, str):
+        st.write(f"{column.title()}: {value.title()}")  # Capitalize string values
+    else:
+        st.write(f"{column.title()}: {value}")  # Display non-string values as is
